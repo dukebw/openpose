7f8b77efc43cf93981ffe1ddf01314d74987373a
Brendan Duke <dukebw@mcmaster.ca>
2019-08-15 21:52:18 -0400

    Save heatmap binaries as numpy.

--- examples/tutorial_api_cpp/07_hand_from_image.cpp
+++ examples/tutorial_api_cpp/07_hand_from_image.cpp
@@ -8,11 +8,12 @@
 // Command-line user intraface
 #define OPENPOSE_FLAGS_DISABLE_PRODUCER
 #define OPENPOSE_FLAGS_DISABLE_DISPLAY
-#include "json.hpp"
-#include <openpose/flags.hpp>
+#include "openpose/flags.hpp"
 // OpenPose dependencies
-#include <openpose/headers.hpp>
-#include <boost/filesystem.hpp>
+#include "openpose/headers.hpp"
+#include "cnpy.h"
+#include "json.hpp"
+#include "boost/filesystem.hpp"
 #include <assert.h>
 #include <glob.h>
 
@@ -20,13 +21,18 @@ namespace boostfs = boost::filesystem;
 
 // Custom OpenPose flags
 // Producer
-DEFINE_string(img_dir, "./",
-    "Process a directory of images. Read all standard formats (jpg, png, bmp, etc.).");
+DEFINE_string(img_dir,
+              "./",
+              "Process a directory of images. Read all standard formats (jpg, "
+              "png, bmp, etc.).");
 DEFINE_string(proposal_dir, "./", "Read bbox proposals.");
+DEFINE_string(sequence_relative_dir,
+              "./",
+              "Relative sequence directory, e.g., "
+              "P05/P05_06/P05_06_10592_pour-sugar-in-cup.");
 DEFINE_string(output_dir, "./", "Output directory.");
 // Display
-DEFINE_bool(no_display,                 false,
-    "Enable to disable the visual display.");
+DEFINE_bool(no_display, false, "Enable to disable the visual display.");
 
 /* NOTE(brendan): source: https://stackoverflow.com/a/17820615 */
 static std::string
@@ -62,9 +68,9 @@ static void
 cropFrame(cv::Mat& handImage,
           const cv::Mat& cvInputData,
           const op::Rectangle<float>& handRectangle,
-          const int netInputSide,
+          int netInputSide,
           const op::Point<int>& netOutputSize,
-          const bool mirrorImage)
+          bool mirrorImage)
 {
         try
         {
@@ -95,7 +101,9 @@ cropFrame(cv::Mat& handImage,
 static void
 display(const std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>& datumsPtr,
         const cv::Mat& imageToProcess,
-        const boostfs::path& outpath)
+        const boostfs::path& outpath,
+        size_t handidx,
+        size_t personidx)
 {
     try
     {
@@ -105,11 +113,12 @@ display(const std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>& datumsPt
         if (datumsPtr != nullptr && !datumsPtr->empty())
         {
                 auto& handHeatMaps = datumsPtr->at(0)->handHeatMaps;
-                const auto num_people = handHeatMaps[0].getSize(0);
-                const auto num_joints = handHeatMaps[0].getSize(1);
-                const auto height = handHeatMaps[0].getSize(2);
-                const auto width = handHeatMaps[0].getSize(3);
-                cv::Mat imageToRender = cv::Mat::zeros(cv::Size{height, width}, CV_8UC3);
+                auto& hand_heatmap = handHeatMaps[handidx];
+                const auto num_people = hand_heatmap.getSize(0);
+                const auto num_joints = hand_heatmap.getSize(1);
+                const auto height = hand_heatmap.getSize(2);
+                const auto width = hand_heatmap.getSize(3);
+                cv::Mat agg_heatmap = cv::Mat::zeros(cv::Size{height, width}, CV_8UC1);
                 for (int32_t jointidx = 0;
                      jointidx < num_joints;
                      ++jointidx) {
@@ -117,38 +126,29 @@ display(const std::shared_ptr<std::vector<std::shared_ptr<op::Datum>>>& datumsPt
                                 height,
                                 width,
                                 CV_32F,
-                                handHeatMaps[0].getPtr() + jointidx*height*width};
+                                (hand_heatmap.getPtr() +
+                                 personidx*num_joints*height*width +
+                                 jointidx*height*width)};
 
                         cv::Mat desiredChannelHeatMapUint8;
                         desiredChannelHeatMap.convertTo(desiredChannelHeatMapUint8,
                                                         CV_8UC1);
-
-                        // Combining both images
-                        cv::Mat heat_colormap;
-                        cv::applyColorMap(desiredChannelHeatMapUint8,
-                                          heat_colormap,
-                                          cv::COLORMAP_JET);
-                        for (int32_t j = 0;
-                             j < height;
-                             ++j) {
-                                for (int32_t i = 0;
-                                     i < width;
-                                     ++i) {
-                                        if (desiredChannelHeatMapUint8.at<uchar>(j, i) < 8)
-                                                continue;
-                                        imageToRender.at<cv::Point3_<uchar>>(j, i) = 0.5*(imageToRender.at<cv::Point3_<uchar>>(j, i) +
-                                                                                          heat_colormap.at<cv::Point3_<uchar>>(j, i));
-                                }
-                        }
+                        agg_heatmap = cv::max(agg_heatmap, desiredChannelHeatMapUint8);
                 }
 
-                cv::addWeighted(imageToRender,
+                cv::Mat img_to_render;
+                cv::applyColorMap(agg_heatmap,
+                                  img_to_render,
+                                  cv::COLORMAP_JET);
+
+                cv::addWeighted(img_to_render,
                                 0.5,
                                 imageToProcess,
                                 0.5,
                                 0.,
-                                imageToRender);
-                cv::imwrite(outpath.c_str(), imageToRender);
+                                img_to_render);
+                bool ret = cv::imwrite(outpath.c_str(), img_to_render);
+                assert(ret);
         }
         else
         {
@@ -248,10 +248,40 @@ void configureWrapper(op::Wrapper& opWrapper)
     }
 }
 
-int tutorialApiCpp()
+static op::Rectangle<float>
+get_rect(const nlohmann::json& bbox)
 {
-    try
-    {
+        float bbox_len = op::fastMax(bbox.at(2), bbox.at(3));
+        float halflen = bbox_len/2.f;
+        bbox_len += halflen;
+        float x0 = op::fastMax(static_cast<float>(bbox.at(0)) - halflen, 0.f);
+        float y0 = op::fastMax(static_cast<float>(bbox.at(1)) - halflen, 0.f);
+
+        return op::Rectangle<float>{x0, y0, bbox_len, bbox_len};
+}
+
+static boostfs::path
+addsuffix(const boostfs::path& outpath, const std::string& suffix)
+{
+        boostfs::path newpath{outpath};
+        newpath += boostfs::path{suffix};
+
+        return newpath;
+}
+
+/**
+ * NOTE(brendan): Take in a subdirectory that is full of images, e.g.,
+ * /path/to/starter-kit-action-recognition/data/interim/rgb_train_segments/P05/P05_06/P05_06_10592_pour-sugar-in-cup
+ * and emit all the hand pose heatmaps into
+ * FLAGS_output_dir/P05/P05_06/P05_06_10592_pour-sugar-in-cup/heatmaps.
+ * Also visualize the heatmaps in FLAGS_output_dir/P05/P05_06/P05_06_10592_pour-sugar-in-cup/display,
+ * unless FLAGS_no_display is set.
+ *
+ * TODO(brendan): Distinguish left/right hands.
+ */
+int tutorialApiCpp(void)
+{
+    try {
         op::log("Starting OpenPose demo...", op::Priority::High);
         const auto opTimer = op::getTimerInit();
 
@@ -269,24 +299,26 @@ int tutorialApiCpp()
         op::log("Starting thread(s)...", op::Priority::High);
         opWrapper.start();
 
+        boostfs::path propdir{FLAGS_proposal_dir};
+        propdir /= FLAGS_sequence_relative_dir;
+        printf("%s\n", propdir.c_str());
+
         glob_t globbuf;
-        int ret = glob((FLAGS_proposal_dir + "/*.json").c_str(),
-                       GLOB_TILDE,
-                       NULL,
-                       &globbuf);
+        int ret = glob((propdir/"*.json").c_str(), GLOB_TILDE, NULL, &globbuf);
         assert(ret == 0);
 
         // Read image and hand rectangle locations
-        printf("gl_pathc: %lu\n", globbuf.gl_pathc);
         for (size_t pathidx = 0;
              pathidx < globbuf.gl_pathc;
              ++pathidx) {
                 boostfs::path proppath{globbuf.gl_pathv[pathidx]};
                 boostfs::path impath{FLAGS_img_dir};
-                impath += boostfs::path{proppath}.replace_extension("jpg").filename();
+                impath /= FLAGS_sequence_relative_dir;
+                impath /= boostfs::path{proppath}.replace_extension("jpg").filename();
                 boostfs::path outpath{FLAGS_output_dir};
-                outpath += boostfs::path{proppath}.replace_extension("").filename();
-                outpath += boostfs::path{"_heatmap.jpg"};
+                outpath /= FLAGS_sequence_relative_dir;
+                boostfs::create_directories(outpath);
+                outpath /= boostfs::path{proppath}.replace_extension("").filename();
                 printf("impath %s proppath %s outpath %s\n",
                        impath.c_str(),
                        proppath.c_str(),
@@ -299,20 +331,17 @@ int tutorialApiCpp()
                 propifs >> props;
 
                 std::vector<std::array<op::Rectangle<float>, 2>> handRectangles;
-                for (auto const& prop: props) {
+                for (size_t i = 0;
+                     i < props.size();
+                     ++i) {
+                        auto& prop = props[i];
                         assert(prop["bbox"].size() == 4);
 
-                        float bbox_len = op::fastMax(prop["bbox"].at(2), prop["bbox"].at(3));
-                        float halflen = bbox_len / 2.0f;
-                        bbox_len += halflen;
-                        float x0 = op::fastMax(static_cast<float>(prop["bbox"].at(0)) - halflen, 0.f);
-                        float y0 = op::fastMax(static_cast<float>(prop["bbox"].at(1)) - halflen, 0.f);
-                        std::array<op::Rectangle<float>, 2> next_bbox{
-                                op::Rectangle<float>{0.f, 0.f, 0.f, 0.f},
-                                op::Rectangle<float>{x0,
-                                                     y0,
-                                                     bbox_len,
-                                                     bbox_len}};
+                        auto right = get_rect(prop["bbox"]);
+                        auto left = get_rect(prop["bbox"]);
+
+                        std::array<op::Rectangle<float>, 2> next_bbox{left,
+                                                                      right};
                         handRectangles.push_back(next_bbox);
                 }
 
@@ -327,29 +356,102 @@ int tutorialApiCpp()
 
                 // Process and display image
                 opWrapper.emplaceAndPop(datumsPtr);
-                if (datumsPtr != nullptr)
-                {
-                        datumPtr->netOutputSize.x = 368;
-                        datumPtr->netOutputSize.y = 368;
-                        const auto netInputSide = op::fastMin(datumPtr->netOutputSize.x,
-                                                              datumPtr->netOutputSize.y);
-                        auto& inputNetData = datumsPtr->at(0)->inputNetData[0];
+                assert(datumsPtr != nullptr);
+
+                datumPtr->netOutputSize.x = 368;
+                datumPtr->netOutputSize.y = 368;
+                auto netInputSide = op::fastMin(datumPtr->netOutputSize.x,
+                                                datumPtr->netOutputSize.y);
+                std::vector<int> compression_params;
+                compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+                compression_params.push_back(9);
+                std::vector<uchar> heatmapsbin;
+                std::vector<int64_t> heatmapsbin_sizes;
+                for (size_t rectidx = 0;
+                     rectidx < handRectangles.size();
+                     ++rectidx) {
+                        printf("rectidx %lu\n", rectidx);
+                        auto& handrect = handRectangles[rectidx];
+
                         cv::Mat handImage;
+                        auto outpath_rect = addsuffix(outpath,
+                                                      (std::string{"rect"} +
+                                                       std::to_string(rectidx)));
                         cropFrame(handImage,
                                   imageToProcess,
-                                  handRectangles.at(0).at(1),
+                                  handrect.at(0),
+                                  netInputSide,
+                                  datumPtr->netOutputSize,
+                                  true);
+
+                        if (!FLAGS_no_display) {
+                                auto outpath_display = addsuffix(outpath_rect,
+                                                                 "_heatmap_left.jpg");
+                                display(datumsPtr,
+                                        handImage,
+                                        outpath_display,
+                                        0,
+                                        rectidx);
+                        }
+
+                        cropFrame(handImage,
+                                  imageToProcess,
+                                  handrect.at(1),
                                   netInputSide,
                                   datumPtr->netOutputSize,
                                   false);
 
-                        if (!FLAGS_no_display)
-                                display(datumsPtr, handImage, outpath);
+                        if (!FLAGS_no_display) {
+                                auto outpath_display = addsuffix(outpath_rect,
+                                                                 "_heatmap_right.jpg");
+                                display(datumsPtr,
+                                        handImage,
+                                        outpath_display,
+                                        1,
+                                        rectidx);
+                        }
+
+                        auto& handHeatMaps = datumsPtr->at(0)->handHeatMaps;
+                        auto& hand_heatmap = handHeatMaps[1];
+                        const size_t num_joints = hand_heatmap.getSize(1);
+                        auto height = hand_heatmap.getSize(2);
+                        auto width = hand_heatmap.getSize(3);
+                        for (int32_t jointidx = 0;
+                             jointidx < num_joints;
+                             ++jointidx) {
+                                cv::Mat joint_heatmap{
+                                        height,
+                                        width,
+                                        CV_32F,
+                                        hand_heatmap.getPtr() + jointidx*height*width};
+
+                                cv::Mat joint_heatmap_u8;
+                                joint_heatmap.convertTo(joint_heatmap_u8,
+                                                        CV_8UC1);
+
+                                auto outpath_joint = addsuffix(outpath_rect,
+                                                               (std::string{"joint"} +
+                                                                std::to_string(jointidx) +
+                                                                std::string{".png"}));
+
+                                std::vector<uchar> outbuf;
+                                bool ret = cv::imencode(".png",
+                                                        joint_heatmap_u8,
+                                                        outbuf,
+                                                        compression_params);
+                                assert(ret);
+
+                                heatmapsbin.insert(heatmapsbin.end(),
+                                                   outbuf.begin(),
+                                                   outbuf.end());
+                                heatmapsbin_sizes.push_back(outbuf.size());
+                        }
                 }
-                else
-                {
-                        op::log("Image could not be processed.", op::Priority::High);
-                }
-                printf("display\n");
+
+                auto outpath_heatmaps = addsuffix(outpath, "_heatmaps.npy");
+                cnpy::npy_save(outpath_heatmaps.string(), heatmapsbin, "w");
+                auto outpath_hmap_sizes = addsuffix(outpath, "_heatmap_sizes.npy");
+                cnpy::npy_save(outpath_hmap_sizes.string(), heatmapsbin_sizes, "w");
         }
 
         // Info
@@ -361,14 +463,12 @@ int tutorialApiCpp()
 
         // Return
         return 0;
-    }
-    catch (const std::exception& e)
-    {
+    } catch (const std::exception& e) {
         return -1;
     }
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     // Parsing command line flags
     gflags::ParseCommandLineFlags(&argc, &argv, true);
